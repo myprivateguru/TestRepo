fa12cb836206f8610ac194a896793cf138ffe197
somwanshi.dnyaneshwar@gmail.com <Mahatma@352>
2019-09-17 23:50:46 +0530

    commit 1

---
+++ FromStart/src/PackageFirst/MyClass.java
@@ -0,0 +1,16 @@
+package PackageFirst;
+
+class MyClass {
+	static void sayHello() {
+		System.out.println("Helloo Dnyaneshwar !");
+		
+	}
+	public static void main (String[] args) 
+	{
+	    sayHello();    
+	        //this is comment
+	        /***this is comment*/
+	 
+	}
+
+}
